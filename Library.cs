4b969883f0a690235e2d9b9aad51032bcb351e71
Luis Ajucum <luis.ajucumsantos@conduent.com>
2023-05-17 19:11:30 -0600

    First commit

---
+++ Library.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace LibraryManagmentSystem
+{
+public class Library {
+
+private List<Book> books;
+public Library(){
+
+
+}
+
+}
+
+//list of books 
+
+//methods add, remove, and search
+
+}
